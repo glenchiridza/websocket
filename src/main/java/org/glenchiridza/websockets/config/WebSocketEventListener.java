1a4a5de0f0e86118e6d62aaff14430c52d97cb84
glenchiridza <glenchiridz@gmail.com>
2024-04-16 22:20:01 +0200

    event listener

--- src/main/java/org/glenchiridza/websockets/config/WebSocketEventListener.java
+++ src/main/java/org/glenchiridza/websockets/config/WebSocketEventListener.java
@@ -2,7 +2,11 @@ package org.glenchiridza.websockets.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.glenchiridza.websockets.enums.MessageType;
+import org.glenchiridza.websockets.model.RoomMessage;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -11,9 +15,20 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @Slf4j
 public class WebSocketEventListener {
 
+    private final SimpMessageSendingOperations messageTemplate;
+
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event){
-
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if(username != null){
+            log.info("user disconnected :: {}",username);
+            var roomMessage = RoomMessage.builder()
+                    .messageType(MessageType.LEFT)
+                    .sender(username)
+                    .build();
+            messageTemplate.convertAndSend("/room/public",roomMessage);
+        }
     }
 
 }
